9acc7888c7f090ef21a72d143d97653ed1666484
toniyanez <toniyanez72@icloud.com>
2025-03-11 00:31:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ NEWS_API_KEY = os.getenv("a716466bd7ac46688b89c4db5c092065")
 ALPHA_VANTAGE_KEY = os.getenv("PGHAQ75R5BNRQG61")
 TWITTER_BEARER_TOKEN = os.getenv("SjZVNVZadUY3bWRLVllmNnZZTnc6MTpjaQ")
 
+@app.route("/")
+def home():
+    return "API is running!"
+
 @app.route("/api/news", methods=["GET"])
 def get_news():
     url = f"https://newsapi.org/v2/top-headlines?category=business&apiKey={NEWS_API_KEY}"
